2d461556600d9a0d640bec4a3e6b874d868c7787
uttamsutariya <pateluttam171210@gmail.com>
2024-06-19 17:50:23 +0530

    added error handling and authentication

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -6,10 +6,11 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
   return (
-    <form className="space-y-3">
+    <form action={authenticate} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
